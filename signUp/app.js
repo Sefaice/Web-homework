40fc0d758c6c67502dd47694c75dae3782041965
Sefaice <31604315+Sefaice@users.noreply.github.com>
2017-12-20 23:57:55 +0800

    some update of turtuial

---
+++ signUp/app.js
@@ -0,0 +1,25 @@
+/*
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+*/
+
+var express = require('express');
+var fs=require("fs");
+var app = express();
+
+app.use(express.static("./"));
+
+app.listen(3000, function() {   //监听http://127.0.0.1:3000端口
+    console.log("server start");
+});
